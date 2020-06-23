fdc4a926e8964e54d0a963b8ff0466072ddb855a
Tom Hewitt <tommhewitt@gmail.com>
2020-06-23 21:24:10 +0100

    refactor: add an api url to package

--- utils/issues.js
+++ utils/issues.js
@@ -1,24 +1,22 @@
 const fetch = require('node-fetch');
 const chalk = require('chalk');
 const figures = require('figures');
-const issuesUrl = require('../package.json').bugs.url;
+const { apiUrl } = require('../package.json').repository;
 
 const getIssues = (labels) => new Promise(((resolve, reject) => {
-  if (!issuesUrl) {
-    reject(new Error(`${chalk.red(figures.cross)} There is no "bugs: { url : "<url>" }" in your package.json!`));
+  if (!apiUrl) {
+    reject(new Error(`${chalk.red(figures.cross)} There is no "repository: { apiUrl : "<url>" }" in your package.json!`));
   }
 
   if (!labels) {
     reject(new Error(`${chalk.red(figures.cross)} No labels parameter specified for repo issues!`));
   }
 
-  const apiUrl = issuesUrl.replace('github.com', 'api.github.com/repos');
-
-  fetch(apiUrl)
+  fetch(`${apiUrl}/issues`)
     .then((response) => response.json())
     .then((issues) => {
       const filteredIssues = issues.filter((i) => i.labels.every((l) => labels.includes(l.name)));
-      console.log(`${chalk.green(figures.tick)} ${issues.length} issues found to add to release`);
+      console.log(`${chalk.green(figures.tick)} Adding ${issues.length} issues to the release`);
       resolve(filteredIssues);
     });
 }));
