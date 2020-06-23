d34916ed2851bbba2ba490eb8ecb3ca37f23ed10
Tom Hewitt <tommhewitt@gmail.com>
2020-06-23 16:53:23 +0100

    feat: add a commit, tag and push module

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const chalk = require('chalk');
 const getVersion = require('./utils/version');
 const getIssues = require('./utils/issues');
 const getChangelog = require('./utils/changelog');
+const commitAndTag = require('./utils/commit-and-tag');
 
 async function main() {
-  console.log(chalk.magenta('Github Releaser') + chalk.yellow(' by ') + chalk.cyan('Tom Hewitt'));
-  const version = await getVersion('');
-  const issues = await getIssues('bug');
-  await getChangelog(version, issues);
+	console.log(chalk.magenta('Github Releaser') + chalk.yellow(' by ') + chalk.cyan('Tom Hewitt'));
+	const version = await getVersion('');
+	const issues = await getIssues('bug');
+	await getChangelog(version, issues);
+	await commitAndTag('test', false)
 }
 
 main();
