b0896667d9a86d3af6114b64f81a2a7a202c51d9
Tom Hewitt <tommhewitt@gmail.com>
2020-06-23 20:24:26 +0100

    refactor: commit-and-tag

--- utils/commit-and-tag.js
+++ utils/commit-and-tag.js
@@ -1,34 +1,38 @@
 const chalk = require('chalk');
 const figures = require('figures');
-const path = require("path");
+const path = require('path');
 const simpleGit = require('simple-git');
 const branchName = require('current-git-branch');
-const git = simpleGit(path.resolve(__dirname, '..'));
-const branch = branchName(path.resolve(__dirname, '..'))
 
-const { repository: { url } } = require('../package.json');
+const git = simpleGit(path.resolve(__dirname, '..'));
+const branch = branchName(path.resolve(__dirname, '..'));
+
+function success(message) {
+  console.log(`${chalk.green(figures.tick)} ${message}`);
+}
+
+function error(message) {
+  console.log(`${chalk.red(figures.cross)} ${message}`);
+}
 
 async function commitAndTag(version, push) {
+  await git.add(['CHANGELOG.md', 'package.json'])
+    .then(() => success('Staged changed files'))
+    .catch((e) => error(`Could not stage files: ${e.message}`));
 
-	await git.add(['CHANGELOG.md', 'package.json']).catch(e => addError(e));
-	await git.commit('chore(release): ' + version).catch(e => commitError(e))
-	await git.addTag(version).catch(e => tagError(e));
-	
-	if (push) {
-		await git.push('origin', branch).catch(e => console.log(e.message))
-	}
-}
+  await git.commit(`chore(release): ${version}`)
+    .then(() => success('Commited files'))
+    .catch((e) => error(`Could not commit files: ${e.message}`));
 
-function tagError(e) {
-	console.log(`${chalk.red(figures.cross)} Could not add new tag: ${e.message}`)
-}
+  await git.addTag(version)
+    .then(() => success(`Tagged '${version}'`))
+    .catch((e) => error(`Could not tag: ${e.message}`));
 
-function addError(e) {
-	console.log(`${chalk.red(figures.cross)} Could not add files: ${e.message}`)
-}
-
-function commitError(e) {
-	console.log(`${chalk.red(figures.cross)} Could not commit files: ${e.message}`)
+  if (push) {
+    await git.push('origin', branch)
+      .then(() => success('Pushed to origin'))
+      .catch((e) => error(`Could not push to branch '${branch}': ${e.message}`));
+  }
 }
 
 module.exports = commitAndTag;
