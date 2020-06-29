2bf3a05da71bba666951ee771e936d86d195cf6e
Tom Hewitt <tommhewitt@gmail.com>
2020-06-29 21:58:43 +0100

    refactor(#5): tidy code

--- utils/release.js
+++ utils/release.js
@@ -8,44 +8,38 @@ const branch = branchName(cwd);
 const git = simpleGit(cwd);
 const { apiUrl } = require(`${cwd}/package.json`).repository;
 
-async function release(version, changelog, token, dryRun) {
-  if (dryRun) {
-    success('Pushed to origin');
-    success('Pushed tags to origin');
-    success('Created Github release');
-  } else {
-    await git.push('origin', branch)
-      .then(() => success('Pushed to origin'))
-      .catch((e) => error(`Could not push to branch '${branch}': ${e.message}`));
+async function release(version, changelog, token) {
+  await git.push('origin', branch)
+    .then(() => success('Pushed to origin'))
+    .catch((e) => error(`Could not push to branch '${branch}': ${e.message}`));
 
-    await git.pushTags('origin')
-      .then(() => success('Pushed tags to origin'))
-      .catch((e) => error(`Could not push tags': ${e.message}`));
+  await git.pushTags('origin')
+    .then(() => success('Pushed tags to origin'))
+    .catch((e) => error(`Could not push tags': ${e.message}`));
 
-    const releaseBody = {
-      tag_name: version,
-      name: version,
-      body: changelog,
-      draft: false,
-      prerelease: false,
-    };
+  const releaseBody = {
+    tag_name: version,
+    name: version,
+    body: changelog,
+    draft: false,
+    prerelease: false,
+  };
 
-    fetch(`${apiUrl}/releases`, {
-      method: 'post',
-      body: JSON.stringify(releaseBody),
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `token ${token}`,
-      },
-    })
-      .then((res) => {
-        if (!res.ok) {
-          error(`Could not create Github release: ${res.status}`);
-          throw new Error(`Could not create Github release: ${res.status}`);
-        }
-        success('Created Github release');
-      });
-  }
+  fetch(`${apiUrl}/releases`, {
+    method: 'post',
+    body: JSON.stringify(releaseBody),
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `token ${token}`,
+    },
+  })
+    .then((res) => {
+      if (!res.ok) {
+        error(`Could not create Github release: ${res.status}`);
+        throw new Error(`Could not create Github release: ${res.status}`);
+      }
+      success('Created Github release');
+    });
 }
 
 module.exports = release;
