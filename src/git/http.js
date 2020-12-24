ac2d1a2aa9bd407a70e11cae91bbfebc622f9bd0
Tom Hewitt <tommhewitt@gmail.com>
2020-12-24 16:34:57 +0000

    refactor(#31): update issues code

---
+++ src/git/http.js
@@ -0,0 +1,45 @@
+const fetch = require('node-fetch');
+
+module.exports = {
+  async get({ url, token }) {
+    return fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `token ${token}`,
+      },
+    }).then((response) => response.json());
+  },
+
+  async post({ url, body, token }) {
+    return fetch(url, {
+      method: 'POST',
+      body,
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `token ${token}`,
+      },
+    });
+  },
+
+  async patch({ url, body, token }) {
+    return fetch(url, {
+      method: 'PATCH',
+      body,
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `token ${token}`,
+      },
+    }).then((response) => response.json());
+  },
+
+  async remove({ url, token }) {
+    return fetch(url, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `token ${token}`,
+      },
+    }).then((response) => response.json());
+  },
+};
