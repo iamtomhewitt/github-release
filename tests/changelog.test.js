4c40b12211e75b0e627211ca4271cbd89116b75e
Tom Hewitt <tommhewitt@gmail.com>
2020-12-28 22:45:14 +0000

    test(#33): add changelog tests

--- tests/changelog.test.js
+++ tests/changelog.test.js
@@ -1,26 +1,25 @@
-const { createChangelog } = require('../src/changelog');
-const log = require('../src/logger');
 const fs = require('fs');
 const sinon = require('sinon');
+const { createChangelog } = require('../src/changelog');
+const log = require('../src/logger');
 
 describe('changelog', () => {
-  const version = '1.2.3'
+  const version = '1.2.3';
   const issues = [];
 
   const logSuccessSpy = jest.spyOn(log, 'success');
-  const logErrorSpy = jest.spyOn(log, 'error');
   const fsReadSpy = jest.spyOn(fs.promises, 'readFile');
   const fsWriteSpy = jest.spyOn(fs.promises, 'writeFile');
 
   afterEach(() => {
     sinon.restore();
-  })
+  });
 
   it('does not create a changelog in dry run mode', async () => {
     const { changelog } = await createChangelog({ version, issues, dryRun: true });
 
-    expect(changelog).toBe('')
-    expect(logSuccessSpy).toHaveBeenCalledWith('CHANGELOG generated')
+    expect(changelog).toBe('');
+    expect(logSuccessSpy).toHaveBeenCalledWith('CHANGELOG generated');
     expect(fsReadSpy).not.toHaveBeenCalled();
     expect(fsWriteSpy).not.toHaveBeenCalled();
   });
@@ -31,7 +30,7 @@ describe('changelog', () => {
     const { changelog } = await createChangelog({ version, issues, dryRun: false });
 
     expect(changelog).not.toBeNull();
-    expect(logSuccessSpy).toHaveBeenCalledWith('CHANGELOG generated')
+    expect(logSuccessSpy).toHaveBeenCalledWith('CHANGELOG generated');
     expect(fsReadSpy).toHaveBeenCalled();
   });
 
@@ -39,16 +38,16 @@ describe('changelog', () => {
     sinon.stub(fs.promises, 'writeFile');
     const mockIssues = [
       {
-        html_url: "https://github.com/iamtomhewitt/github-releaser/issues/33",
+        html_url: 'https://github.com/iamtomhewitt/github-releaser/issues/33',
         number: 33,
-        title: "Add tests",
-      }
-    ]
+        title: 'Add tests',
+      },
+    ];
 
     const { changelog } = await createChangelog({ version, issues: mockIssues, dryRun: false });
 
     expect(changelog).not.toBeNull();
-    expect(logSuccessSpy).toHaveBeenCalledWith('CHANGELOG generated')
+    expect(logSuccessSpy).toHaveBeenCalledWith('CHANGELOG generated');
     expect(fsReadSpy).toHaveBeenCalled();
   });
 });
