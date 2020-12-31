6b2c053ac6b4f69348a8110f27c83a0d71890515
Tom Hewitt <tommhewitt@gmail.com>
2020-12-31 12:33:57 +0000

    feat(#29): git commit pom.xml as well

--- src/git/index.js
+++ src/git/index.js
@@ -1,4 +1,5 @@
 const simpleGit = require('simple-git');
+const fs = require('fs');
 const log = require('../logger');
 
 module.exports = {
@@ -11,8 +12,14 @@ module.exports = {
     try {
       const cwd = process.cwd();
       const git = simpleGit(cwd);
+      const pomLocation = `${cwd}/pom.xml`;
+      const files = [`${cwd}/CHANGELOG.md`, `${cwd}/package.json`, `${cwd}/package-lock.json`];
 
-      await git.add([`${cwd}/CHANGELOG.md`, `${cwd}/package.json`, `${cwd}/package-lock.json`]);
+      if (fs.existsSync(pomLocation)) {
+        files.push(`${cwd}/pom.xml`);
+      }
+
+      await git.add(files);
       await git.commit(`chore(release): ${version}`);
       await git.addTag(version);
 
