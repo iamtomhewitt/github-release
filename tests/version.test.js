4cdd4599cda0406e8efe9e78cc7837d34c9250e4
Tom Hewitt <tommhewitt@gmail.com>
2021-03-02 21:16:19 +0000

    test(#39): update tests

--- tests/version.test.js
+++ tests/version.test.js
@@ -1,59 +1,55 @@
-const sinon = require('sinon');
-const util = require('util');
 const fs = require('fs');
 const log = require('../src/logger');
+const sinon = require('sinon');
+const util = require('util');
 const { generateVersion, writeVersion } = require('../src/version');
-
 const { version } = require(`${process.cwd()}/package.json`);
 
 util.promisify = jest.fn();
 
 describe('version', () => {
-  const logSuccessSpy = jest.spyOn(log, 'success');
-  const logDryRunSpy = jest.spyOn(log, 'dryRun');
+
+  beforeEach(() => {
+    spyOn(log, 'success')
+    spyOn(log, 'dryRun')
+
+    sinon.restore();
+    jest.clearAllMocks();
+  });
 
   describe('generation', () => {
-    beforeEach(() => {
-      sinon.restore();
-      jest.clearAllMocks();
-    });
 
     it('generates version', async () => {
       const { newVersion } = await generateVersion({});
 
       expect(newVersion).not.toBe(version);
-      expect(logSuccessSpy).toHaveBeenCalled();
+      expect(log.success).toHaveBeenCalled();
     });
 
     it('generates a version with an appendage', async () => {
       const { newVersion } = await generateVersion({ append: '-append' });
 
       expect(newVersion.endsWith('-append')).toBeTruthy();
-      expect(logSuccessSpy).toHaveBeenCalled();
+      expect(log.success).toHaveBeenCalled();
     });
 
     it('generates a version with an override', async () => {
       const { newVersion } = await generateVersion({ override: '1.2.3' });
 
       expect(newVersion).toBe('1.2.3');
-      expect(logSuccessSpy).toHaveBeenCalled();
+      expect(log.success).toHaveBeenCalled();
     });
   });
 
   describe('writing', () => {
     const newVersion = '1.2.3';
 
-    beforeEach(() => {
-      sinon.restore();
-      jest.clearAllMocks();
-    });
-
     it('writes version', async () => {
       sinon.stub(fs.promises, 'writeFile');
 
       await writeVersion({ newVersion, dryRun: false });
 
-      expect(logSuccessSpy).toHaveBeenCalledWith('Wrote 1.2.3 to package.json and package-lock.json');
+      expect(log.success).toHaveBeenCalledWith('Wrote 1.2.3 to package.json and package-lock.json');
     });
 
     it('does not write version in dry run mode', async () => {
@@ -61,8 +57,8 @@ describe('version', () => {
 
       await writeVersion({ newVersion, dryRun: true });
 
-      expect(logSuccessSpy).not.toHaveBeenCalled();
-      expect(logDryRunSpy).toHaveBeenCalledWith('Wrote 1.2.3 to package.json and package-lock.json');
+      expect(log.success).not.toHaveBeenCalled();
+      expect(log.dryRun).toHaveBeenCalledWith('Wrote 1.2.3 to package.json and package-lock.json');
     });
   });
 });
